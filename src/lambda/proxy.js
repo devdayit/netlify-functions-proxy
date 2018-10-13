c7dce9525cd47cc90c6d8726116eaa1438bab793
Gianluca Esposito <g@esposi.to>
2018-10-13 10:16:20 +0200

    Add Meetup proxy function

---
+++ src/lambda/proxy.js
@@ -0,0 +1,29 @@
+const { SIG_ID, SIGS, GROUP_IDS } = process.env;
+
+import fetch from "node-fetch";
+
+import flatten from "lodash/flatten";
+
+const getEvents = ({ groupId, signatureKey }) => {
+  const endpoint = `https://api.meetup.com/${groupId}/events?photo-host=public&page=20&sig_id=${SIG_ID}&sig=${signatureKey}`;
+  return fetch(endpoint).then(response => response.json());
+};
+
+exports.handler = async (event, context) => {
+  const signatures = SIGS.split(",");
+  const groupIds = GROUP_IDS.split(",");
+
+  const promises = signatures.map((signature, i) =>
+    getEvents({
+      groupId: groupIds[i],
+      signatureKey: signature
+    })
+  );
+
+  return Promise.all(promises)
+    .then(data => ({
+      statusCode: 200,
+      body: JSON.stringify(flatten(data))
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
