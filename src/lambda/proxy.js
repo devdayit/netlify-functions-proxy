6ecdccf36c67b4b85b13ae3a1a6aa17aa34fbfc9
Gianluca Esposito <g@esposi.to>
2021-09-25 10:42:10 +0200

    Update proxy.js

--- src/lambda/proxy.js
+++ src/lambda/proxy.js
@@ -7,28 +7,29 @@ import flatten from "lodash/flatten";
 const getEvents = ({ groupId }) => {
   const endpoint = `https://api.meetup.com/${groupId}/events?photo-host=public&page=20`;
   return fetch(endpoint, {
-    headers: { Authorization: `Bearer ${ACCESS_TOKEN}` }
-  }).then(response => response.json());
+    headers: { Authorization: `Bearer ${ACCESS_TOKEN}` },
+  }).then((response) => response.json());
 };
 
 exports.handler = async (event, context) => {
   const groupIds = GROUP_IDS.split(",");
 
-  const promises = groupIds.map(groupId =>
+  const promises = groupIds.map((groupId) =>
     getEvents({
-      groupId: groupId
+      groupId: groupId,
     })
   );
 
   return Promise.all(promises)
-    .then(data => ({
+    .then((data) => ({
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers":
-          "Origin, X-Requested-With, Content-Type, Accept"
+          "Origin, X-Requested-With, Content-Type, Accept",
+        "Access-Control-Allow-Methods": "*",
       },
       statusCode: 200,
-      body: JSON.stringify(flatten(data))
+      body: JSON.stringify(flatten(data)),
     }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
